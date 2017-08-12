2667391f46ee136914adef09c356e8e2147ea56e
Ian Storz <istorz@google.com>
2017-08-12 10:59:31 -0700

    make properties acceptance test use mirage

--- tests/acceptance/properties/index-test.js
+++ tests/acceptance/properties/index-test.js
@@ -4,9 +4,13 @@ import moduleForAcceptance from 'environweb/tests/helpers/module-for-acceptance'
 moduleForAcceptance('Acceptance | properties/index');
 
 test('visiting /properties', function(assert) {
+  const properties = server.createList('property', 3);
+
   visit('/properties');
 
   andThen(function() {
     assert.equal(currentURL(), '/properties');
+    assert.equal(find('li').length, 3);
+    assert.equal(find('li:first').text(), properties[0].name);
   });
 });
