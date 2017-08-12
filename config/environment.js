241ccf0d9ea8c9a4226369961254eeb30affbf4e
Ian Storz <istorz@google.com>
2017-08-12 09:20:23 -0700

    configure podModulePrefix

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'environweb',
+    podModulePrefix: 'environweb/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
