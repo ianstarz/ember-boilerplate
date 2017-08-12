5bf8a620611bfc2324b6e2c847259f256c160807
Ian Storz <istorz@google.com>
2017-08-12 10:28:06 -0700

    fix unused variable lint error in mirage/config.js

--- mirage/config.js
+++ mirage/config.js
@@ -2,7 +2,7 @@ export default function() {
   this.namespace = 'api';
   this.timing = 200;
 
-  this.get('/properties', (schema, request) => {
+  this.get('/properties', (schema) => {
     return schema.properties.all();
   });
 }
